d35865dcad6f63e62e4f11f7094343b643d4f399
AR Ali's Mac <macbookair@MacBook-Air.local>
2025-08-06 16:22:48 +0500

    Made some updates

--- src/VideoSection/VideoSection.jsx
+++ src/VideoSection/VideoSection.jsx
@@ -3,18 +3,22 @@ import React from "react";
 const VideoSection = () => {
   return (
     <section className="bg-black text-white z-50 relative">
-  <div className="w-full">
-    <video
-      className="w-full h-auto max-h-[90vh] object-cover"
-      autoPlay
-      muted
-      loop
-    >
-      <source src="/Assets/video.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
-  </div>
-</section>
+      <div className="w-full">
+        <video
+          className="w-full h-auto max-h-[90vh] object-cover"
+          autoPlay
+          muted
+          loop
+          playsInline
+          controls={false}
+          disablePictureInPicture
+          controlsList="nodownload nofullscreen noremoteplayback"
+        >
+          <source src="/Assets/video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </section>
   );
 };
 
